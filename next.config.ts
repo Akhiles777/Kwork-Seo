f40e5a52e3473e43c82fabbc6692611061cda580
Gasan <gasanalunkachev@gmail.com>
2026-02-01 20:28:48 +0300

    ff

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   trailingSlash: true, 
   output: 'export',  
-
-
-   images: {
-  domains: ['i.postimg.cc'],
-    formats: ['image/webp'],
-  },
-  
-  
-
-
 };
 
 export default nextConfig;
